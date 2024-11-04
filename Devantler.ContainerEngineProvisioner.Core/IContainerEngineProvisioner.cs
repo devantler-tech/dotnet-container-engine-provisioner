dcf8bc2a0bb7631483bb143639ae6f2c28ebcde5
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-11-04 16:29:11 +0100

    Refactor method signatures in IContainerEngineProvisioner to use default CancellationToken parameters

--- Devantler.ContainerEngineProvisioner.Core/IContainerEngineProvisioner.cs
+++ Devantler.ContainerEngineProvisioner.Core/IContainerEngineProvisioner.cs
@@ -8,34 +8,34 @@ public interface IContainerEngineProvisioner
   /// <summary>
   /// Checks if the container engine is ready.
   /// </summary>
-  /// <param name="token"></param>
-  Task<bool> CheckReadyAsync(CancellationToken token);
+  /// <param name="cancellationToken"></param>
+  Task<bool> CheckReadyAsync(CancellationToken cancellationToken = default);
 
   /// <summary>
   /// Creates a registry in the container engine.
   /// </summary>
   /// <param name="name"></param>
   /// <param name="port"></param>
-  /// <param name="token"></param>
   /// <param name="proxyUrl"></param>
-  Task CreateRegistryAsync(string name, int port, CancellationToken token, Uri? proxyUrl = null);
+  /// <param name="cancellationToken"></param>
+  Task CreateRegistryAsync(string name, int port, Uri? proxyUrl = default, CancellationToken cancellationToken = default);
 
   /// <summary>
   /// Deletes a registry in the container engine.
   /// </summary>
   /// <param name="name"></param>
-  /// <param name="token"></param>
-  Task DeleteRegistryAsync(string name, CancellationToken token);
+  /// <param name="cancellationToken"></param>
+  Task DeleteRegistryAsync(string name, CancellationToken cancellationToken = default);
 
   /// <summary>
   /// Gets the container ID of a container in the container engine.
   /// </summary>
   /// <param name="name"></param>
-  /// <param name="token"></param>
-  Task<string> GetContainerIdAsync(string name, CancellationToken token);
+  /// <param name="cancellationToken"></param>
+  Task<string> GetContainerIdAsync(string name, CancellationToken cancellationToken = default);
 
   /// <summary>
   /// Checks that the container exists in the container engine.
   /// </summary>
-  Task<bool> CheckContainerExistsAsync(string name, CancellationToken token);
+  Task<bool> CheckContainerExistsAsync(string name, CancellationToken cancellationToken = default);
 }
